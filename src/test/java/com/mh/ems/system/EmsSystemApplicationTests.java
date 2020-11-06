ff49348bbfdd8526b19df2f41e62f576e28913bf
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2020-11-06 13:36:47 +0530

    ems-system project

---
+++ src/test/java/com/mh/ems/system/EmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.ems.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
